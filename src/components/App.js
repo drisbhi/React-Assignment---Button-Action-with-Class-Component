45185070dbfeb1132ce88352272888a9d11db30e
sahib bajaj <bajajsaib3@gmail.com>
2021-03-02 18:53:59 +0530

    ans

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,11 @@ class App extends Component {
     render() {
     	return(
     		<div id="main">
-				<button id = "click" onClick ={this.showPara}>Show Para</button>
-				{ this.state.isParaShow ?( <p id = "para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-                </p>): null }
+				<button id = "click" onClick = {this.showPara}> Show Paragraph </button>
+				{ this.state.isParaShow ? ( <p id = "para">
+					 Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+				</p> 
+				) :  null }
     		</div>
     	);
     }
