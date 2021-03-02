e7604369c8742fcf4abbb564b10da9065ed661c2
sahib bajaj <bajajsaib3@gmail.com>
2021-03-02 16:45:37 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state ={
+			isParaShow : false 
+		};
 	};
-
+ showPara = () =>{
+    this.setState( {isParaShow : true});
+ }
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id = "click" onClick ={this.showPara}>Show Para</button>
+				{ this.state.isParaShow ?( <p id = "para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+</p>): null }
     		</div>
     	);
     }
